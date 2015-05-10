90cf2d4dccee7dc568edebddf89eab3ef9817d25
Mustafa Akdeniz <rodeomusty@hotmail.com>
2015-05-10 15:12:59 +0300

    1 sn için ayarlandi

--- timer0.c
+++ timer0.c
@@ -2,18 +2,19 @@
 #fuses HS,NOWDT,NOPROTECT,NOLVP
 #use delay(clock=8000000)
 
+//timer hesap uygulamasi 
+//http://www.libstock.com/projects/view/398/timer-calculator
+
 #INT_RTCC
 void zaman(void){
    output_toggle(PIN_E0);
-   set_timer0(0);
+   set_timer0(3036);
 }
 
 void main() {
-    setup_timer_0(RTCC_INTERNAL | RTCC_DIV_128);
+    setup_timer_0(RTCC_INTERNAL | RTCC_DIV_32);
     enable_interrupts(INT_RTCC);
     enable_interrupts(GLOBAL);
-    set_timer0(0);
-    while(1){
-        
-    }
+    set_timer0(3036);
+    while(1){}
 }
