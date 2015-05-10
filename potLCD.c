433dfdb9b9f6194590efa009433edb360494350f
Mustafa Akdeniz <rodeomusty@hotmail.com>
2015-05-10 16:21:26 +0300

    lcd ve potansiyometre

---
+++ potLCD.c
@@ -0,0 +1,16 @@
+#include <18F452.h>
+#DEVICE ADC=10
+#fuses HS,NOWDT,NOPROTECT,NOLVP
+#use delay(clock=8000000)
+#include "flex_lcd.c"
+
+void main() {
+    lcd_init();
+    setup_adc_ports(AN0);
+    setup_adc(ADC_CLOCK_INTERNAL);
+    set_adc_channel(0);
+    while(1){
+    printf(lcd_putc,"Sayi=%ld",read_adc());
+    delay_ms(100);
+    }
+}
